f58908d72eed65d951473a9035717f4befa3f863
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2016-12-01 23:51:41 -0600

    urllib2; csv
    Adding two libraries and putting in an example function for how to
    iterate through data in a csv file given through a url.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+import urllib2
 import database.py
 
 def main():
@@ -13,7 +15,7 @@ def main():
             shares = input(Number of shares: )
             price = input(Purchase price per share: )
             insert(database, (symbol, company, shares, price))
-            
+
         if choice == d:
             remove = input('Enter the ticker symbol of the stock to remove: ')
             delete(database, remove)
@@ -22,14 +24,14 @@ def main():
             file = input('Load file: ')
             database = sqlite3.connect('portfolio.dat')
             database.execute('drop table if exists students')
-            database.execute('create table students (ticker TEXT PRIMARY KEY, company TEXT, shares NEAR, initial REAL, current REAL)')  
+            database.execute('create table students (ticker TEXT PRIMARY KEY, company TEXT, shares NEAR, initial REAL, current REAL)')
 
         if choice == u:
             print('Update stock prices (<Return> to keep current values)...')
 
         if choice == r:
             sort = input('Sort output by (N)ame, or (V)alue? ').lower()
-            
+
             if sort == v:
                 retrieve(database, currentValue)
 
@@ -38,14 +40,20 @@ def main():
 
         if choice == q:
             save = input('Save {} (y/n)? '.format(file))
-            
+
             if save.lower() == y:
 
             if save.lower() == n:
 
             print('Bye!')
             flag = false
+
     
     url = 'http://finance.yahoo.com/d/quotes.csv?s={}&f=sl1d1t1c1ohgv&e=.csv'.format()
-   
+    response = urllib2.urlopen(url)
+    cr = csv.reader(response)
+
+    for row in cr:
+        print row
+
 if __name__== '__main__': main()
