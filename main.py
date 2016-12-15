ebf61387491ece9777aa99628e0676ef12435439
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2016-12-14 20:59:34 -0600

    Final comments
    Yay!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Python 2061-70 FINAL PROJECT
 #Michael Albrecht, Chris Payne, Taylor Jordan
-#Written 11/29/16-12/12/16
+#Written 11/29/16-12/15/16
 #This program is designed to let a user enter any number
 #of stocks to a list. They can be removed, or updated.
 #You can either start a new list or load a saved older
@@ -23,8 +23,9 @@ def main():
         choice = input("\n(A)dd/(D)elete stocks, (L)oad file, (U)pdate prices, (R)eport, or (Q)uit? ").lower()
 
         #Condtional for choice 'A' or 'a'.
-        #Adds a stock to portfolio.dat
+        #Adds a stock to stocks list
         if choice == 'a':
+            #Getting variable from the user.
             print('Add a stock to your portfolio!\n')
             symbol = input('Ticker: ').upper()
             company = input('Company name: ')
@@ -32,8 +33,10 @@ def main():
             pPrice = float(input('Purchase price per share: $'))
             pPrice = int(pPrice*100)
 
+            #Converting internet data to a list
             url = ("http://finance.yahoo.com/d/quotes.csv?s={}&f=sl1d1t1c1ohgv&e=.csv ".format(symbol))
             content = urlopen(url).read().decode().split('\n')
+
             #Loop for getting the current stock value. If the stock is a real NYSE Stock
             #it will pull the value. If stock is fictional it will set initial to current
             for k in content:
@@ -50,23 +53,27 @@ def main():
 
             gl = (-1 + ( (cValue) / (iValue) ))*100
 
+            #Gathering all of the variables in a temp list
+            #and storing it to the stocks list
             temp = [symbol, company, shares, pPrice, cPrice, cValue, gl]
             stocks.append(temp)
 
         #Conditional for choice 'D' or 'd'
-        #Removes a stock from portfolio.dat
+        #Removes a stock from stocks list
         if choice == 'd':
             remove = input('Enter the ticker symbol of the stock to remove: ')
 
+            #Searching the list for the ticker the user entered and removing it.
             for index, each in enumerate(stocks):
                 if remove.upper() == each[0]:
                     stocks.pop(index)
 
         #Conditional for choice 'L' or 'l'
-        #Loads the intialized portfolio.dat
+        #Loads the intialized stocks list
         if choice == 'l':
+            #Prompting the user for the file name, connecting to that file,
+            #getting all the data from the file and storing it to the stocks list
             iFile = input('Load file: ')
-            tempFile = iFile
             db = sqlite3.connect(iFile)
             cursor = db.cursor()
             cursor.execute("SELECT * FROM portfolio")
@@ -76,42 +83,47 @@ def main():
                 stocks.append(r)
 
         #Conditional for choice 'U' or 'u'
-        #Updates the current values of the stocks in portfolio.dat
+        #Updates the current values of the stocks in stocks list
         if choice == 'u':
             temp = []
             print('\nUpdating stock prices...')
+            #Iterating through the list to update the prices
             for o, p in enumerate(stocks):
                 url = ("http://finance.yahoo.com/d/quotes.csv?s={}&f=sl1d1t1c1ohgv&e=.csv ".format(stocks[o][0]))
                 content = urlopen(url).read().decode().split('\n')
                 for k in content:
                     stockData = k.split(',')
+                    #Converting the content of the stocks list to a list to we can use the values
                     p = list(p)
                     if len(stockData) == 9:
+                        #Try to get the price from the internet and store it to a temp list
                         try:
                             p[4] = float(stockData[1])*100
                             temp.append(p)
                             print('{}: {}'.format(p[0], (p[4]/100)))
+                        #If that fails tell the user so and store the initial price to a temp list
                         except:
                             print('A current price for {} could not be found.'.format(stocks[o][0]))
                             p[4] = stocks[o][3]
                             temp.append(p)
+            #Store the temp list to the stocks list and tell the user it's done
             stocks = temp
             print('Stock prices updated!')
 
         #Conditional for choice 'R' or 'r'
-        #Prints out the portfolio.dat in required format based on
+        #Prints out the stocks list in required format based on
         #another set of conditionals for 'Name' or 'Value'
         if choice == 'r':
             sort = input('Sort output by (N)ame, or (V)alue? ').lower()
 
             #Sub-Conditional for choice 'V' or 'v'
-            #Prints out portfolio.dat in value-order
+            #Prints out stocks list in value-order
             if sort == 'v':
                 vSort = sorted(stocks, key=itemgetter(5))
                 printTable(vSort)
 
             #Sub-Conditional for choice 'N' or 'n'
-            #Prints out portfolio.dat in name-order
+            #Prints out stocks list in name-order
             if sort == 'n':
                 nSort = sorted(stocks, key=itemgetter(1))
                 printTable(nSort)
@@ -124,6 +136,7 @@ def main():
             #Conditional for choice 'Y' or 'y'
             #For saving the datatbase
             if save.lower() == 'y':
+
                 #Conditional for when their is no such file.
                 if iFile == "":
                     fName = input('Save file as: ')
@@ -142,7 +155,7 @@ def main():
             print('Bye!')
             flag = False
 
-#Method for printing the top of the table
+#Method for printing the table
 def printTable(stockSort):
     totalCurrentValue = 0
     totalInitialValue = 0
@@ -159,11 +172,12 @@ def printTable(stockSort):
     print('{0: <47}'.format('') + '{0: >8}'.format(int(totalCurrentValue)) + '{:8.1f}%'.format(totalGL))
     print('{0: <50}'.format('') +'{:16}'.format('==============='))
 
-#Metjod for creating the table in the database
+#Metjod for creating and saving the table in the database
 def executeDatabase(db, stockList):
     db.execute('drop table if exists portfolio')
     db.execute('create table portfolio (ticker TEXT PRIMARY KEY, company TEXT, shares NEAR, initial NEAR, current NEAR, value REAL, gl REAL)')
     for row in stockList:
         db.execute('insert into portfolio (ticker, company, shares, initial, current, value, gl) values (?, ?, ?, ?, ?, ?, ?)', (row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
     db.commit()
+
 if __name__== '__main__': main()
